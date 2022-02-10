e49bf7583dda8f8ec3b4626d2419e045b89e73f4
TechiePi <techie@techiepi.dev>
2022-02-10 10:15:59 +0100

    chore: do not use unwrap on add_entry

--- src/server/routes/api/add_entry.rs
+++ src/server/routes/api/add_entry.rs
@@ -4,16 +4,17 @@ use uuid::Uuid;
 use crate::{AnalyticsData, file};
 
 #[put("/add", data = "<data>")]
-pub fn add_entry(data: Json<AnalyticsData>) {
+pub fn add_entry(data: Json<AnalyticsData>) -> Result<(), std::io::Error> {
     let mut clean_data = data;
     let uid = Uuid::new_v4().to_string();
     let epoch = SystemTime::now()
         .duration_since(UNIX_EPOCH)
-        .unwrap()
+        .expect("Clock is going backwards, why tho?")
         .as_millis();
 
     clean_data.id = uid.as_str();
     clean_data.epoch = epoch as usize;
 
-    file::write_file_epoch_and_uid(&(epoch as usize), &uid, clean_data.into_inner()).unwrap();
+    file::write_file_epoch_and_uid(&(epoch as usize), &uid, clean_data.into_inner())?;
+    Ok(())
 }
